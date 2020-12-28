53476b5968fb15fe7094398b7a474a9ebbbfb8f7
akenofu <karim.hm.hussein@gmail.com>
2020-12-28 14:46:10 +0200

    Player Stats

---
+++ Assets/Scripts/LevelManger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManger : MonoBehaviour
+{
+    private Player player;
+    private PlayerStats playerStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerDies()
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float health;
+    public float dashCoolDown;
+    public float superFireBallCooldown, fireBallCooldown;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float dmg)
+    {
+
+    }
+
+}
