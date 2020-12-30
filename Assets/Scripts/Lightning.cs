8968acc1d7fb9706fb3a5b3e5efe374f11241513
akenofu <karim.hm.hussein@gmail.com>
2020-12-30 15:47:50 +0200

    Finalized Aravos Skills and base stats

---
+++ Assets/Scripts/Lightning.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+    public PlayerStats playerStats;
+    public float damage;
+    public int duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Destroy());
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerStats.TakeDamage(damage);
+            Destroy(gameObject, 0.0f);
+        }
+    }
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(gameObject, 0.0f);
+
+    }
+
+}
